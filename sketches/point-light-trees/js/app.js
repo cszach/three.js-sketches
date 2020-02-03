0a65d3b8d81905d6b73f605c8c3ee155cbe2fec3
Nguyen Hoang Duong <you_create@protonmail.com>
2020-02-03 13:30:33 +0700

    point-light-trees: Update directory structure

--- sketches/point-light-trees/js/app.js
+++ sketches/point-light-trees/js/app.js
@@ -1,16 +1,19 @@
+/* global createjs */
+
 // GLOBALS
 
 let scene, camera, renderer, controls, clock;
-let tree;
+let trees, numberOfTrees = 10;
 
 // Scene, camera & renderer settings
 
 const background = new THREE.Color( 0xdddddd );
 const fog = new THREE.FogExp2( 0xdddddd, 0.08 );
 let canvas = document.querySelector( '#app' );
-let context = canvas.getContext( 'webgl2', { alpha: false } );
-let antialias = true;
-let fov = 75, aspect = canvas.clientWidth / canvas.clientHeight, near = 0.1, far = 1000;
+let canvasWidth = canvas.clientWidth, canvasHeight = canvas.clientHeight;
+const context = canvas.getContext( 'webgl2', { alpha: false } );
+const antialias = true;
+let fov = 75, aspect = canvasWidth / canvasHeight, near = 0.1, far = 1000;
 
 if ( THREE.WEBGL.isWebGL2Available() ) {
 
@@ -50,33 +53,11 @@ function init() {
 
 	// TREES
 
-	tree = new PLTree(); // eslint-disable-line no-undef
-
-	tree.setAnimation( function () {
-
-		let time = clock.getElapsedTime();
-
-		this.forEachBranch( function ( i, branch, leaf, light ) {
-
-			light.visible = ( ( i - Math.trunc( time ) % 12 ) % 12 === 0 );
-			leaf.material.setValues( {
-
-				emissive: ( light.visible ) ? light.color.getHex() : 0x000000
-
-			} );
-
-		} );
-
-	} );
-
-	tree.plant( 0, 0, 0, false );
-	scene.add( tree );
-
+	trees = new Array();
 
 	// CreateJS setup
 
-	createjs.Ticker.timingMode = createjs.Ticker.RAF;
-
+	createjs.Ticker.framerate = 60;
 	renderer.setAnimationLoop( render );
 
 }
@@ -87,13 +68,11 @@ function render() {
 
 	if ( canvas.width !== canvas.clientWidth || canvas.height !== canvas.clientHeight ) {
 
+		canvasWidth = canvas.clientWidth;
+		canvasHeight = canvas.clientHeight;
 		onCanvasResize();
 
 	}
-
-	// In-callback animations
-
-	tree.animate();
 
 	// Updates
 
@@ -105,16 +84,12 @@ function render() {
 
 }
 
-function animate() {
-
-	tree.grow();
-
-}
+function animate() { }
 
 function onCanvasResize() {
 
-	renderer.setSize( canvas.clientWidth, canvas.clientHeight );
-	camera.aspect = canvas.clientWidth / canvas.clientHeight;
+	camera.aspect = canvasWidth / canvasHeight;
 	camera.updateProjectionMatrix();
+	renderer.setSize( canvasWidth, canvasHeight );
 
 }
